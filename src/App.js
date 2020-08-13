1b84ce5c1167569a7f5c63fb3642737e7173bc65
blue <textblueboy@gmail.com>
2020-08-13 22:54:40 +0530

    replaced classes with functions

--- src/App.js
+++ src/App.js
@@ -9,34 +9,24 @@ import Brightness5Icon from '@material-ui/icons/Brightness5'
 // dark theme
 import Brightness2Icon from '@material-ui/icons/Brightness2';
 import { ThemeProvider } from "@material-ui/styles";
-import React from 'react';
+import React, { useState } from 'react';
 
 
-export default class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			isDarkTheme: false,
-		}
+export default function App() {
+	const [isDarkTheme, setDarkTheme] = useState(false)
+	const toggleTheme = () => {
+		setDarkTheme(!isDarkTheme)
 	}
 
-	toggleTheme = () => {
-		this.setState({
-			isDarkTheme: !this.state.isDarkTheme,
-		})
-	}
-
-	render() {
-		return (
-			<ThemeProvider theme={this.state.isDarkTheme ? darkTheme : lightTheme} >
-				<CssBaseline />
-				<Grid container justify="flex-end">
-					<IconButton onClick={this.toggleTheme}>
-						{this.state.isDarkTheme ? <Brightness5Icon style={{fontSize:'50px'}}/> : <Brightness2Icon style={{fontSize:'50px'}}/>}
-					</IconButton>
-				</Grid>
-				<Counter />
-			</ThemeProvider>
-		);
-	}
+	return (
+		<ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} >
+			<CssBaseline />
+			<Grid container justify="flex-end">
+				<IconButton onClick={toggleTheme}>
+					{isDarkTheme ? <Brightness5Icon style={{ fontSize: '50px' }} /> : <Brightness2Icon style={{ fontSize: '50px' }} />}
+				</IconButton>
+			</Grid>
+			<Counter />
+		</ThemeProvider>
+	)
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import RemoveCircleRoundedIcon from '@material-ui/icons/RemoveCircleRounded';
 import ReplayIcon from '@material-ui/icons/Replay';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 const styles = {
@@ -22,77 +21,66 @@ const styles = {
 		justifyContent: 'center',
 	}
 }
-export default class Counter extends React.Component {
 
-	state = {
-		count: 0 
-	};
+export default function Counter() {
 
-	increment = () => {
-		this.setState({
-			count: this.state.count + 1
-		});
-	};
+	const [count, setCount] = useState(0)
+	useEffect(() =>{
+		document.addEventListener("keydown", handleKeyDown, false)
 
-	decrement = () => {
-		if (this.state.count > 0) {
-			this.setState({
-				count: this.state.count - 1
-			});
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown, false)
 		}
-	};
-	reset = () => {
-		this.setState({
-			count: 0
-		})
-	}
+	})
 
-	handleKeyDown = (event) => {
+
+	const increment = () => setCount(count + 1)
+	const decrement = () => {
+		if (count > 0) {
+			setCount(count - 1)
+		}
+	}
+	const reset = () => setCount(0)
+
+	const handleKeyDown = (event) => {
 		if (event.keyCode === 75) {
-			this.increment()
+			increment()
 		}
 		if (event.keyCode === 74) {
-			this.decrement()
+			decrement()
 		}
 		if (event.keyCode === 27) {
-			this.reset()
+			reset()
 		}
 	}
 
-	componentDidMount() {
-		document.addEventListener("keydown", this.handleKeyDown, false);
-	}
-
-	render() {
-		return (
-
-			<div>
-				<Grid>
-					<h1 style={styles.header} >{this.state.count}</h1>
+	return (
+		<div>
+			<Grid>
+				<h1 style={styles.header} >{count}</h1>
+			</Grid>
+			<Grid style={styles.buttons} container direction="row" justify="center" alignItems="center">
+				<Grid item xs style={{ textAlign: 'center' }}>
+					<IconButton onClick={increment}>
+						<AddCircleRoundedIcon style={styles.largeButton} />
+					</IconButton>
 				</Grid>
-				<Grid style={styles.buttons} container direction="row" justify="center" alignItems="center">
-					<Grid item xs style={{textAlign : 'center'}}>
-						<IconButton onClick={this.increment}>
-							<AddCircleRoundedIcon style={styles.largeButton} />
-						</IconButton>
-					</Grid>
 
-					<Grid item xs style={{textAlign : 'center'}}>
-						<IconButton textAlign='center' onClick={this.decrement}>
-							<RemoveCircleRoundedIcon style={styles.largeButton} />
-						</IconButton>
-		
-					</Grid>
-
-					<Grid item xs style={{textAlign : 'center'}}>
-						<IconButton onClick={this.reset}>
-							<ReplayIcon style={styles.largeButton} />
-						</IconButton>
-					</Grid>
+				<Grid item xs style={{ textAlign: 'center' }}>
+					<IconButton textAlign='center' onClick={decrement}>
+						<RemoveCircleRoundedIcon style={styles.largeButton} />
+					</IconButton>
 
 				</Grid>
-			</div>
-		)
-	}
+
+				<Grid item xs style={{ textAlign: 'center' }}>
+					<IconButton onClick={reset}>
+						<ReplayIcon style={styles.largeButton} />
+					</IconButton>
+				</Grid>
+
+			</Grid>
+		</div>
+	)
+
 }
-
