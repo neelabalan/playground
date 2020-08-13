c390486cf4944a5076aa081065d83c325dfab6b2
blue <textblueboy@gmail.com>
2020-08-13 22:56:01 +0530

    removed unwanted imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import ThemeProvider from '@material-ui/core/styles';
+
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
