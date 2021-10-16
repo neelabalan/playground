9169e63a39a91a17961bab0a5e5fdd0fdcce7fd5
neelabalan <neelabalan.n@outlook.com>
2021-10-16 14:44:19 +0530

    duplicate entry with indices

--- jsondb.py
+++ jsondb.py
@@ -1,10 +1,15 @@
 import json
 import os
+from json import JSONDecodeError
 from typing import List, Dict, Callable
 from pathlib import Path
 
 
-class PathDoesNotExist(Exception):
+class PathNotFoundError(Exception):
+    pass
+
+
+class DuplicateEntryError(Exception):
     pass
 
 
@@ -18,9 +23,9 @@ def load(dbpath):
     collections["root"] = path
 
     if not path.exists():
-        raise PathDoesNotExist("The provided path does not exists")
+        raise PathNotFoundError("The provided path does not exists")
     if not path.is_dir():
-        raise Exception("Path to directory is not provided")
+        raise Exception("Path to *directory* is not provided")
 
     for path in path.iterdir():
         collections[path.stem] = path.resolve()
@@ -32,13 +37,13 @@ class jsondb:
         self.collections = collections
         self.dbpath = collections.get("root")
 
-    def get(self, name: str):  
+    def get(self, name: str, index=[]):
         if self.collections:
-            return collection(self.collections.get(name))
+            return collection(self.collections.get(name), index)
         else:
             raise Exception("collections not loaded")
 
-    def new(self, name):
+    def new(self, name, indices=[]):
         if not type(name) == str:
             raise TypeError('collection name must be of type "str"')
         collection_path = self.dbpath / "{}.json".format(name)
@@ -46,22 +51,23 @@ class jsondb:
             json.dump([], jsonfile)
         # update collections
         self.collections[name] = collection_path
-        return collection(collection_path)
+        return collection(collection_path, indices=indices)
 
-    def drop(self, collection):  
+    def drop(self, collection):
         # delete collection
         collection.drop()
         return self.collections.pop(str(collection))
 
 
 class collection:
-    def __init__(self, path):
+    def __init__(self, path, indices=[]):
         self.path = path
+        self.indices = indices
         try:
             with open(self.path, "r") as jsonfile:
                 self.collection = json.load(jsonfile)
-        except Exception as e:
-            print("Error loading JSON document: {}".format(e))
+        except JSONDecodeError as decode_error:
+            print("Error loading JSON document: {}".format(decode_error))
 
     def __str__(self):
         return self.path.stem
@@ -82,11 +88,26 @@ class collection:
             else:
                 return list(filter(predicate, self.collection))
         else:
-            raise Exception("arument provided is not callable")
+            raise TypeError("arument provided is not callable")
+
+    def duplicate_entry_exists(self, document):
+        if not self.indices:
+            return False
+
+        flag = False
+        for index in self.indices:
+            doc = self.find(lambda x: x.get(index) == document[0].get(index))
+            if doc:
+                flag = True
+        return flag
 
     def insert(self, document: List) -> List[Dict]:
-        self.collection.extend(document)
-        return document
+        if not self.duplicate_entry_exists(document):
+            self.collection.extend(document)
+            self.dump()
+            return document
+        else:
+            raise DuplicateEntryError("something")
 
     def delete(self, predicate: Callable) -> List[Dict]:
         # use index to remove with pop in list
@@ -94,17 +115,25 @@ class collection:
         popped_docs = list()
         for index, document in documents:
             popped_docs.append(self.collection.pop(index))
+        self.dump()
         return popped_docs
 
     def update(self, func, predicate) -> List[Dict]:
         documents = self.find(predicate, return_index=True)
         for index, document in documents:
             # func should return a dict
-            self.collection[index] = func(document)  
+            self.collection[index] = func(document)
+        self.dump()
 
-    def dump(self):
+    def dump(self, indent=False):
         with open(self.path, "w") as jsonfile:
-            json.dump(self.collection, jsonfile)
+            if indent:
+                json.dump(self.collection, jsonfile, indent=4)
+            else:
+                json.dump(self.collection, jsonfile)
 
-    def dumps(self):
-        return json.dumps(self.collection)
+    def dumps(self, indent=False):
+        if indent:
+            return json.dumps(self.collection)
+        else:
+            return json.dumps(self.collection, indent=4)
--- test_jsondb.py
+++ test_jsondb.py
@@ -2,107 +2,118 @@ import pytest
 import os
 import json
 from jsondb import jsondb
+from jsondb.jsondb import DuplicateEntryError
+
 # utils
 def dummy_func():
 	pass
 
+
 def test_iscallable():
 	assert jsondb.is_callable(dummy_func)
 	assert not jsondb.is_callable(int)
-	assert not jsondb.is_callable('hello')
+	assert not jsondb.is_callable("hello")
 
 
 # jsondb
 @pytest.fixture
 def db():
-	return jsondb.load('db')
+	return jsondb.load("db")
+
 
 def test_load():
 	# this works
-	db = jsondb.load('db')
+	db = jsondb.load("db")
 
 	# this should not
 	with pytest.raises(Exception) as e:
-		db = jsondb.load('/doesnotexist')
+		db = jsondb.load("/doesnotexist")
+
 
 def test_get(db):
-	assert db.get('products')
+	assert db.get("products")
+
 
 def test_new_and_drop(db):
-	test = db.new('test')
-	assert test.path	
+	test = db.new("test")
+	assert test.path
 	db.drop(test)
 	assert not os.path.exists(test.path)
 
+
 def test_insert_and_dumps(db):
-	doc = [{'key': 'value'}]
-	test = db.new('test')
-	#insert
+	doc = [{"key": "value"}]
+	test = db.new("test")
+	# insert
 	test.insert(doc)
-	assert test.find(lambda x: x['key']=='value') == doc
+	assert test.find(lambda x: x["key"] == "value") == doc
 	# dump
 	test.dump()
 	# load and check
-	with open(test.path, 'r') as file:
+	with open(test.path, "r") as file:
 		json.load(file) == doc
+
 
 # collection
 def test_find(db):
-	restaurant = db.get('restaurant')
-	assert restaurant.find(lambda x: x.get("address")== "228 City Road") == \
-	[{
-		"URL": "http://www.just-eat.co.uk/restaurants-cn-chinese-cardiff/menu",
-		"_id": {
-			"$oid": "55f14312c7447c3da7051b26"
-		},
-		"address": "228 City Road",
-		"address line 2": "Cardiff",
-		"name": ".CN Chinese",
-		"outcode": "CF24",
-		"postcode": "3JH",
-		"rating": 5,
-		"type_of_food": "Chinese"
-	}]
+	restaurant = db.get("products")
+	assert restaurant.find(lambda x: x.get("price") == 200) == [
+		{
+			"_id": "ac3",
+			"name": "AC3 Phone",
+			"brand": "ACME",
+			"type": "phone",
+			"price": 200,
+			"rating": 3.8,
+			"warranty_years": 1,
+			"available": True,
+		}
+	]
+
 
 def test_delete(db):
-	restaurant = db.get('restaurant')
-	doc = [{
-		"URL": "http://www.just-eat.co.uk/restaurants-cn-chinese-cardiff/menu",
-		"_id": {
-			"$oid": "55f14312c7447c3da7051b26"
-		},
-		"address": "228 City Road",
-		"address line 2": "Cardiff",
-		"name": ".CN Chinese",
-		"outcode": "CF24",
-		"postcode": "3JH",
-		"rating": 5,
-		"type_of_food": "Chinese"
-	}]
+	test = db.get("test")
+	doc = [
+		{
+			"URL": "http://www.just-eat.co.uk/restaurants-cn-chinese-cardiff/menu",
+			"_id": {"$oid": "55f14312c7447c3da7051b26"},
+			"address": "228 City Road",
+			"address line 2": "Cardiff",
+			"name": ".CN Chinese",
+			"outcode": "CF24",
+			"postcode": "3JH",
+			"rating": 5,
+			"type_of_food": "Chinese",
+		}
+	]
 	# document present
-	assert restaurant.find(lambda x: x.get("address")== "228 City Road") == doc 
+	test.insert(doc)
+	assert test.find(lambda x: x.get("address") == "228 City Road") == doc
 
 	# delete the document
-	restaurant.delete(lambda x: x.get("address")== "228 City Road") 
+	test.delete(lambda x: x.get("address") == "228 City Road")
 
 	# check if it is not there
-	assert not restaurant.find(lambda x: x.get("address")== "228 City Road") == doc 
+	assert not test.find(lambda x: x.get("address") == "228 City Road") == doc
 	# since no dump is called changes not written to file
+	db.drop(test)
 
 
 def test_update(db):
-	doc = [
-		{'key': 'value1'}, 
-		{'key': 'value2'}
-	]
-	test = db.new('test')
+	doc = [{"key": "value1"}, {"key": "value2"}]
+	test = db.new("test")
 	test.insert(doc)
+
 	def func(document):
-		document.update({'key': 'newvalueadded'})
+		document.update({"key": "newvalueadded"})
 		return document
 
-	test.update(func, lambda x: x['key'] == 'value2')
-	assert test.collection ==  [
-		{'key': 'value1'}, 
-		{'key': 'newvalueadded'}
-	]
+	test.update(func, lambda x: x["key"] == "value2")
+	assert test.collection == [{"key": "value1"}, {"key": "newvalueadded"}]
+	db.drop(test)
+
+def test_duplicate_entry(db):
+	test = db.new("test", indices=['key'])
+	test.insert([{'key': 'value'}])
+	with pytest.raises(DuplicateEntryError) as e:
+		test.insert([{'key': 'value'}])
