c873225f446ed5d5d50a8ea8adc1e6f75ceca5d8
neelabalan <neelabalan.n@outlook.com>
2022-01-28 14:31:23 +0530

    fuzzy search tag, titles and colored preview

--- urlr.py
+++ urlr.py
@@ -1,20 +1,18 @@
-import datetime
 import json
 import os
 import pathlib
-import shutil
-import string
 import subprocess
 import sys
 import tempfile
 import urllib.parse
 import urllib.request
-from typing import Dict, List, Optional
 
+import pyskim
 import toml
 import typer
 from bs4 import BeautifulSoup
 from jsondb import DuplicateEntryError, jsondb
+from rich import print
 from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
@@ -22,75 +20,29 @@ from rich.style import Style
 from rich.text import Text
 
 app = typer.Typer()
-date_format = "%a %d %b %Y %X"
-
-TOMLEXT = ".toml"
-command = string.Template("$editor $filename")
-fuzzy_search_command = string.Template(
-    'echo -n "$options" | sk -m --color="prompt:27,pointer:27" --preview="urlr preview {}" --preview-window=up:50%'
-)
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
-}
-
-console = Console()
-title_style = Style(color="grey74")
-url_style = Style(color="blue", underline=True)
-description_style = Style(color="white")
+db = None
 
 
-# template
-"""
-[[-]]
-url = ''
-title = ''
-description= ''
-tags = []
-"""
-bookmark_template = {"-": [{"url": "", "title": "", "description": "", "tags": []}]}
+class RichStyles:
+    title_style = Style(color="grey74", bold=True)
+    url_style = Style(color="blue", underline=True)
 
 
-def environ_present(key="EDITOR"):
-    return key in os.environ
-
-
-def format_date(dtms):
-    seconds, micros = divmod(dtms, 1000000)
-    days, seconds = divmod(seconds, 86400)
-    bmdate = datetime.datetime(1601, 1, 1) + datetime.timedelta(days, seconds, micros)
-    return bmdate.strftime(date_format)
-
-
-def open_temp_toml_file(template=bookmark_template):
-    if environ_present("EDITOR"):
-        editor = os.environ["EDITOR"]
-        fd, filename = tempfile.mkstemp(suffix=TOMLEXT, text=True)
-        with open(filename, "w") as file:
-            toml.dump(template, file)
-        write_status = subprocess.call(
-            command.substitute(editor=editor, filename=filename), shell=True
-        )
-        if write_status != 0:
-            os.remove(filename)
-        return filename, write_status
-    else:
-        raise Exception("EDITOR not found in env")
+def open_temp_toml_file(template={"-": [{"url": "", "title": "", "tags": []}]}):
+    fd, filename = tempfile.mkstemp(suffix=".toml", text=True)
+    with open(filename, "w") as file:
+        toml.dump(template, file)
+    write_status = subprocess.call("$EDITOR {}".format(filename), shell=True)
+    return filename, write_status
 
 
 def format_text(bookmark):
     newline = Text("\n\n", justify="center")
     bookmark_text = Text(justify="center")
-    bookmark_text.append_text(Text(bookmark.get("title"), style=title_style))
+    bookmark_text.append_text(Text(bookmark.get("title"), style=RichStyles.title_style))
     bookmark_text.append_text(newline)
 
-    if bookmark.get("description"):
-        bookmark_text.append_text(
-            Text(bookmark.get("description"), style=description_style)
-        )
-        bookmark_text.append_text(newline)
-
-    bookmark_text.append(Text(bookmark.get("url"), style=url_style))
+    bookmark_text.append(Text(bookmark.get("url"), style=RichStyles.url_style))
     bookmark_text.append_text(newline)
 
     tags = bookmark.get("tags")
@@ -98,66 +50,66 @@ def format_text(bookmark):
     tags = " ── ".join(colored_tags)
     return Panel(
         bookmark_text,
-        title=str(bookmark.get("_id")),
-        title_align="left",
-        subtitle=tags + " ── " + bookmark.get("added_date"),
+        subtitle=tags,
         subtitle_align="right",
         padding=1,
     )
 
 
-def display_bookmark(bookmarks: List[Dict]):
+def display_bookmark(bookmarks):
     if bookmarks:
         for bookmark in bookmarks:
-            console.print("\n\n")
-            console.print(format_text(bookmark))
+            print("\n\n")
+            print(format_text(bookmark))
 
 
-def deduce_title(url: str) -> str:
-    request = urllib.request.Request(url, headers=headers)
+def get_website_title(url):
+    request = urllib.request.Request(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+        },
+    )
     web = BeautifulSoup(urllib.request.urlopen(request), features="html.parser")
     return web.title.string
 
 
-def is_valid_url(url: str) -> bool:
+def validate_insert(bookmark):
+    url = bookmark.get("url")
+    if not url:
+        print("[red bold]url not added[/]")
+        sys.exit()
+
     result = urllib.parse.urlparse(url)
-    return all([result.scheme, result.netloc])
+    if not all([result.scheme, result.netloc]):
+        print("[red bol]url not valid[/]")
+        sys.exit()
+    return bookmark
 
 
 def insert(bookmarks):
     total_bookmark = len(bookmarks.get("-"))
     insert_count = 0
     for bookmark in bookmarks.get("-"):
-        url = bookmark.get("url")
-        if not url:
-            console.print("[red bold]url not added")
-            sys.exit()
-        if not is_valid_url(url):
-            console.print("[red bol]url not valid")
-            sys.exit()
+        bookmark = validate_insert(bookmark)
         try:
             db.insert(
                 [
                     {
                         "url": url,
                         "title": bookmark.get("title")
-                        or deduce_title(bookmark.get("url"))
-                        or "---",
-                        "description": bookmark.get("description"),
+                        or get_website_title(bookmark.get("url")),
                         "tags": bookmark.get("tags"),
-                        "added_date": bookmark.get("added_date")
-                        or datetime.datetime.now().strftime(date_format),
                     }
                 ]
             )
             insert_count += 1
         except DuplicateEntryError as err:
             console.print("[red]Duplicate url found - {}".format(url))
-    console.print(
-        "[green bold]{}/{} {} added".format(
+    print(
+        "[green bold]{}/{} bookmark(s) added".format(
             insert_count,
             total_bookmark,
-            "bookmark" if total_bookmark == 1 else "bookmarks",
         )
     )
 
@@ -172,26 +124,15 @@ def get_bookmarks_sorted():
     return ordered_latest
 
 
-def fuzzy_search(options):
-    options = "\n".join(options)
-    selected = subprocess.Popen(
-        fuzzy_search_command.substitute(options=options),
-        shell=True,
-        stdout=subprocess.PIPE,
-    ).communicate()[0]
-    selected = selected.decode("utf-8")
-    return list(filter(None, selected.split("\n")))
-
-
 def distinct_tags():
-    tags = set()
-    notes = db.find(lambda x: True)
-    for note in notes:
-        tags.update(note.get("tags"))
-    return list(tags)
+    tags = list()
+    urls = db.find(lambda x: True)
+    for url in urls:
+        tags.extend(url.get("tags"))
+    return sorted(list(set(tags)))
 
 
-def distinct_titles():
+def titles():
     notes = db.find(lambda x: True)
     return [note.get("title") for note in notes]
 
@@ -208,6 +149,7 @@ def new():
 
 @app.command()
 def preview(title: str):
+    console = Console(color_system="256")
     bookmark = db.find(lambda x: x.get("title") == title)
     if bookmark:
         console.print(format_text(bookmark[0]))
@@ -215,20 +157,14 @@ def preview(title: str):
 
 @app.command()
 def tag():
-    tags = fuzzy_search(distinct_tags())
+    tags = pyskim.skim(distinct_tags(), '-m --color="prompt:27,pointer:27"')
     bookmarks = db.find(lambda x: set(tags).issubset(set(x.get("tags"))))
-    display_bookmark(bookmarks)
-
-
-@app.command()
-def tags():
-    content = []
-    for tag in distinct_tags():
-        total = len(db.find(lambda x: tag in x.get("tags")))
-        content.append(
-            "[black on blue] {} [/][black on grey93] {} [/]\t".format(tag, total)
-        )
-    console.print(Columns(content, expand=True, equal=True))
+    titles = [bookmark["title"] for bookmark in bookmarks]
+    bookmark = pyskim.skim(
+        titles,
+        '-m --color="prompt:27,pointer:27" --preview="urlr preview {}" --preview-window=up:50%',
+    )
+    display_bookmark(bookmark)
 
 
 @app.command()
@@ -241,20 +177,17 @@ def find(searchstr: str):
 
 
 @app.command()
-def ls(order: str = typer.Argument("recent"), val: int = typer.Argument(10)):
-    if order not in ["recent", "past"]:
-        raise Exception('order has to be either "recent" or "past"')
-
+def ls(val: int = typer.Argument(10)):
     bookmarks = get_bookmarks_sorted()
-    if order == "recent":
-        display_bookmark(bookmarks[:val])
-    else:
-        display_bookmark(bookmarks[-val:])
+    display_bookmark(bookmarks[:val])
 
 
 @app.command()
 def edit():
-    title = fuzzy_search(distinct_titles())
+    title = pyskim.skim(
+        title(),
+        '-m --color="prompt:27,pointer:27" --preview="urlr preview {}" --preview-window=up:50%',
+    )
 
     def update(document):
         if document:
@@ -263,7 +196,6 @@ def edit():
                     "url": document.get("url"),
                     "title": document.get("title"),
                     "tags": document.get("tags"),
-                    "description": document.get("description"),
                 }
             )
             if status == 0:
@@ -284,9 +216,7 @@ def rm():
 
 @app.command("import")
 def import_viv():
-    """import from vivaldi bookmarks"""
-    home = str(pathlib.Path.home())
-    fullpath = home + "/.config/vivaldi/Default/Bookmarks"
+    fullpath = str(pathlib.Path.home()) + "/.config/vivaldi/Default/Bookmarks"
     bookmarks = {"-": list()}
     with open(fullpath) as browserbm:
         jsondict = json.loads(browserbm.read())
@@ -298,23 +228,32 @@ def import_viv():
                     "url": element.get("url"),
                     "title": element.get("name"),
                     "tags": ["browser"],
-                    "description": "",
-                    "added_date": format_date(int(element.get("date_added"))),
                 }
             )
         insert(bookmarks)
 
 
-def init_db():
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    if ctx.invoked_subcommand is None:
+        url_titles = pyskim.skim(
+            titles(),
+            '-m --color="prompt:27,pointer:27,marker:28" --preview="urlr preview {}" --preview-window=up:50%',
+        )
+        if url_titles:
+            for title in url_titles:
+                url = db.find(lambda x: x.get("title") == title)
+                subprocess.call(
+                    ["xdg-open", url[0].get("url")],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+
+
+def run():
+    global db
     dbroot = pathlib.Path.home() / ".local/urlr"
     dbroot.mkdir(parents=True, exist_ok=True)
-    collection_name = "urlr"
     db = jsondb(str(pathlib.Path(dbroot / "urlr.json")))
     db.set_index("url")
-    db.set_index("title")
-    return db
-
-
-db = init_db()
-if __name__ == "__main__":
     app()
