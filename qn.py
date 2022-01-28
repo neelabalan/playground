29c3fd2b9817fe639e5edd7cda62c3a77d908904
neelabalan <neelabalan.n@outlook.com>
2022-01-28 19:10:03 +0530

    pyskim added, new and today command removed

--- qn.py
+++ qn.py
@@ -4,22 +4,20 @@ import hashlib
 import json
 import os
 import pathlib
-import shutil
-import string
 import subprocess
 import sys
 import tempfile
-from typing import Optional
 
+import pyskim
 import toml
 import typer
 from cryptography.fernet import Fernet, InvalidToken
 from jsondb import jsondb
 from rich import print
+from rich.console import Console
 from rich.markdown import Markdown
 from rich.panel import Panel
 from rich.style import Style
-from rich.table import Table
 from rich.text import Text
 
 app = typer.Typer()
@@ -41,42 +39,19 @@ class Paths:
     enc_path = pathlib.Path.home() / ".local/qn/qn.enc"
 
 
-def environ_present(key="EDITOR"):
-    return key in os.environ
-
-
 def open_temp_md_file(
-    title="",
-    tags=[],
-    created_date=datetime.datetime.now().strftime(date_format),
+    template={
+        "title": "",
+        "tags": [],
+        "created_date": datetime.datetime.now().strftime(date_format),
+    },
     mdtext="",
 ):
-    command = string.Template("$editor $filename")
-    if environ_present("EDITOR"):
-        editor = os.environ["EDITOR"]
-        fd, filename = tempfile.mkstemp(suffix=".md", text=True)
-        with open(filename, "w") as file:
-
-            file.write(
-                "+++\n{}+++{}".format(
-                    toml.dumps(
-                        {
-                            "title": title,
-                            "tags": tags,
-                            "created_date": created_date,
-                        }
-                    ),
-                    mdtext,
-                )
-            )
-        write_status = subprocess.call(
-            command.substitute(editor=editor, filename=filename), shell=True
-        )
-        if write_status != 0:
-            os.remove(filename)
-        return filename, write_status
-    else:
-        raise Exception("EDITOR not found in env")
+    fd, filename = tempfile.mkstemp(suffix=".md", text=True)
+    with open(filename, "w") as file:
+        file.write(SEPERATOR + "\n" + toml.dumps(template) + SEPERATOR + mdtext)
+    write_status = subprocess.call("$EDITOR {}".format(filename), shell=True)
+    return filename, write_status
 
 
 def parse_front_matter(filepath):
@@ -98,11 +73,11 @@ def parse_front_matter(filepath):
 
 
 def distinct_tags():
-    tags = set()
+    tags = list()
     notes = db.find(lambda x: True)
     for note in notes:
-        tags.update(note.get("tags"))
-    return list(tags)
+        tags.extend(note.get("tags"))
+    return sorted(list(set(tags)))
 
 
 def distinct_titles():
@@ -120,36 +95,23 @@ def extract_md_text(filepath):
             return strlist[2]
 
 
-def render_table(notes):
-    table = Table("title", "tags", "created_date", title="quick notes", expand=True)
-    for note in notes:
-        # tags = note.get("tags")
-        # colored_tags = ", ".join(map(lambda x: "[black on blue]#" + x + "[/]", tags))
-        table.add_row(
-            note.get("title"),
-            ", ".join(note.get("tags")) or "-",
-            note.get("created_date"),
+def display_note(note):
+    console = Console(color_system="256")
+    tags = Text()
+    for tag in note.get("tags"):
+        tags.append("#{}".format(tag), style=RichStyles.tag)
+        tags.append(" ─ ")
+
+    console.print(
+        Panel(
+            Markdown(note.get("note") or ">", code_theme="ansi_dark"),
+            title=note.get("title"),
+            title_align="center",
+            subtitle=tags + Text(note.get("created_date")),
+            subtitle_align="right",
         )
-    print(table)
-
-
-def display_notes(notes):
-    for note in notes:
-        tags = Text()
-        for tag in note.get("tags"):
-            tags.append("#{}".format(tag), style=RichStyles.tag)
-            tags.append("  ")
-
-        print(
-            Panel(
-                Markdown(note.get("note") or ">", code_theme="ansi_dark"),
-                title=note.get("title"),
-                title_align="center",
-                subtitle=tags + Text(note.get("created_date")),
-                subtitle_align="right",
-            )
-        )
-        print("\n")
+    )
+    print("\n")
 
 
 def filter_notes_by_tags(tags):
@@ -165,122 +127,37 @@ def get_all_notes_ordered(reverse=True):
     )
 
 
-def fuzzy_search(options):
-    fuzzy_search_command = string.Template(
-        'echo -n "$options" | sk -m --color="prompt:27,pointer:27" --preview="qn preview {}" --preview-window=up:50%'
-    )
-    options = "\n".join(options)
-    selected = subprocess.Popen(
-        fuzzy_search_command.substitute(options=options),
-        shell=True,
-        stdout=subprocess.PIPE,
-    ).communicate()[0]
-    selected = selected.decode("utf-8")
-    return list(filter(None, selected.split("\n")))
-
-
 @app.command()
 def preview(title: str):
     note = db.find(lambda x: x.get("title") == title)
-    display_notes(note)
+    display_note(note[0])
 
 
 @app.command()
-def today():
-    title = datetime.datetime.now().strftime("%d%b%Y")
-    note = db.find(lambda x: x.get("title") == title)
-    if note:
-        edit(title)
-    else:
-        filename, status = open_temp_md_file(title=title)
-        front_matter = parse_front_matter(filename)
-        db.insert(
-            [{**parse_front_matter(filename), **{"note": extract_md_text(filename)}}]
+def tag():
+    tags = pyskim.skim(
+        distinct_tags(),
+        '-m --ansi --bind="ctrl-a:select-all"',
+    )
+    if tags:
+        notes = db.find(lambda x: set(tags).issubset(set(x.get("tags"))))
+        titles = [note["title"] for note in notes]
+        title = pyskim.skim(
+            titles,
+            '--color="prompt:27,pointer:27" --preview="qn preview {}" --preview-window=up:50%',
         )
-        print("[green]new note added")
+        if title:
+            note = db.find(lambda x: x.get("title") == title[0])
+            display_note(note[0])
 
 
 @app.command()
-def new(title: str):
-    note = db.find(lambda x: x.get("title") == title)
-    if note:
-        edit(title)
-    else:
-        filename, status = open_temp_md_file(title=title)
-        front_matter = parse_front_matter(filename)
-        front_matter["title"] = front_matter["title"] or title
-        db.insert(
-            [{**parse_front_matter(filename), **{"note": extract_md_text(filename)}}]
-        )
-        print("[green]new note added")
-
-
-@app.command()
-def edit():
-    def update(document):
-        if document:
-            filename, status = open_temp_md_file(
-                note["title"], note["tags"], note["created_date"], mdtext=note["note"]
-            )
-            with open(filename, "r") as file:
-                updated_front_matter = parse_front_matter(filename)
-                updated_text = extract_md_text(filename)
-                document.update(**updated_front_matter, **{"note": updated_text})
-                return document
-
+def view():
     title = fuzzy_search(distinct_titles())
     if title:
         note = db.find(lambda x: x.get("title") == title[0])
         if note:
-            note = note[0]
-            db.update(update, lambda x: x.get("title") == title[0])
-        else:
-            print("[red]no note found with this title")
-
-
-@app.command()
-def tag(tagstr: str):
-    if tagstr:
-        tags = list(map(str.strip, tagstr.split(",")))
-        notes = filter_notes_by_tags(tags)
-        (notes)
-    else:
-        tag = fuzzy_search(distinct_tags())
-        if tag:
-            notes = filter_notes_by_tags(tag)
-            display_notes(notes)
-
-
-@app.command()
-def lss(order: str = typer.Argument("recent"), val: int = typer.Argument(5)):
-    if order not in ["recent", "last"]:
-        raise Exception('order has to be either "first" or "last"')
-    all_notes = get_all_notes_ordered()
-    if order == "first":
-        render_table(all_notes[:val])
-    else:
-        render_table(all_notes[-val:])
-
-
-@app.command()
-def ls(order: str = typer.Argument("recent"), val: int = typer.Argument(5)):
-    if order not in ["recent", "last"]:
-        raise Exception('order has to be either "first" or "last"')
-    all_notes = get_all_notes_ordered()
-    notes = []
-    if order == "first":
-        display_notes(all_notes[:val])
-    else:
-        display_notes(all_notes[-val:])
-
-
-@app.command()
-def show():
-    title = fuzzy_search(distinct_titles())
-    if title:
-        note = db.find(lambda x: x.get("title") == title[0])
-        if note:
-            display_notes(note)
+            display_note(note)
 
 
 @app.command()
@@ -292,7 +169,10 @@ def find(searchstr: str):
 
 @app.command()
 def rm():
-    title = fuzzy_search(distinct_titles())
+    title = pyskim.skim(
+        distinct_titles(),
+        '--ansi --preview="qn preview {}" --preview-window=up:50% --bind="ctrl-a:select-all"',
+    )
     if title:
         deleted_doc = db.delete(lambda x: x.get("title") == title[0])
         if deleted_doc:
@@ -339,10 +219,57 @@ def export(path: str):
     pass
 
 
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    def update(document):
+        if document:
+            filename, status = open_temp_md_file(
+                {
+                    "title": note["title"],
+                    "tags": note["tags"],
+                    "created_date": note["created_date"],
+                },
+                mdtext=note["note"],
+            )
+            with open(filename, "r") as file:
+                updated_front_matter = parse_front_matter(filename)
+                updated_text = extract_md_text(filename)
+                document.update(**updated_front_matter, **{"note": updated_text})
+                return document
+
+    if not ctx.invoked_subcommand:
+        title = pyskim.skim(
+            distinct_titles(),
+            '--ansi --print-query --print-cmd --preview="qn preview {}" --preview-window=up:50% --bind="ctrl-a:select-all"',
+        )
+        if title:
+            title = title[-1]
+            note = db.find(lambda x: x.get("title") == title)
+            if note:
+                note = note[0]
+                db.update(update, lambda x: x.get("title") == title)
+            else:
+                filename, status = open_temp_md_file(
+                    {
+                        "title": title,
+                        "tags": [],
+                        "created_date": datetime.datetime.now().strftime(date_format),
+                    }
+                )
+                front_matter = parse_front_matter(filename)
+                front_matter["title"] = front_matter["title"] or title
+                db.insert(
+                    [
+                        {
+                            **parse_front_matter(filename),
+                            **{"note": extract_md_text(filename)},
+                        }
+                    ]
+                )
+                print("[green]new note added")
+
+
 def run():
-    if not shutil.which("sk"):
-        print("[bold red]could not find sk in path")
-        print("install from https://github.com/lotabout/skim")
     if not Paths.enc_path.exists():
         global db
         db = jsondb(str(Paths.note_path))
