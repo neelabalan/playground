d3e4c8a3be8a6843f692ef86978a46f410b93079
neelabalan <neelabalan.n@outlook.com>
2023-02-11 14:38:19 +0530

    reactivity

---
+++ misc/spy_on_change.py
@@ -0,0 +1,32 @@
+# https://nedbatchelder.com/blog/202206/adding_a_dunder_to_an_object.html
+
+class SomeObject:
+    ...
+
+class Nothing:
+    """Just to get a nice repr for Nothing."""
+    def __repr__(self):
+        return "<Nothing>"
+
+obj = SomeObject()
+obj.attr = "first"
+print(obj.attr)
+
+def spy_on_changes(obj):
+    """Tweak an object to show attributes changing."""
+    class Wrapper(obj.__class__):
+        def __setattr__(self, name, value):
+            old = getattr(self, name, Nothing())
+            print(f"Spy: {name}: {old!r} -> {value!r}")
+            return super().__setattr__(name, value)
+    obj.__class__ = Wrapper
+
+spy_on_changes(obj)
+obj.attr = "second"
+# Spy: attr: 'first' -> 'second'
+
+print(obj.attr)
+# 'second'
+
+obj.another = 'foo'
+# Spy: another: <Nothing> -> 'foo'
