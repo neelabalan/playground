179e094cb42410db3b8f82848d94df36f8c78eed
neelabalan <neelabalan.n@outlook.com>
2023-07-19 15:16:43 +0530

    property based testing with reports

---
+++ test_report/test_source_file.py
@@ -0,0 +1,38 @@
+"""
+`venv/bin/python -m pytest --csv tests.csv`
+
+`pytest --html=report.html --self-contained-html`
+
+`pytest -s --hypothesis-verbosity=verbose`
+"""
+from hypothesis import given
+from hypothesis import strategies as st
+
+from source_file import add
+from source_file import multiply
+from source_file import divide
+
+
+@given(
+    st.floats(allow_nan=False, allow_infinity=False),
+    st.floats(allow_nan=False, allow_infinity=False, min_value=1e-6),
+)
+def test_add_property(a, b):
+    assert add(a, b) == a + b
+
+
+@given(
+    st.floats(allow_nan=False, allow_infinity=False),
+    st.floats(allow_nan=False, allow_infinity=False, min_value=1e-6),
+)
+def test_multiply_property(a, b):
+    assert multiply(a, b) == a * b
+
+
+@given(
+    st.floats(allow_nan=False, allow_infinity=False),
+    st.floats(allow_nan=False, allow_infinity=False, min_value=1e-6),
+)
+def test_divide_property(a, b):
+    if b != 0:
+        assert divide(a, b) == a / b
