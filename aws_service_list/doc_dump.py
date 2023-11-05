f6367cae9fa288d2e7a5b18db0e09cf42c364f7e
neelabalan <neelabalan.n@outlook.com>
2023-11-05 22:32:38 +0530

    AWS service list and docs links export

---
+++ aws_service_list/doc_dump.py
@@ -0,0 +1,86 @@
+import re
+
+import json
+from typing import List
+from typing import Dict
+from urllib.request import urlopen
+from xml.etree import ElementTree
+
+import requests
+
+base_url = "https://docs.aws.amazon.com"
+
+
+def get_href_links(url: str) -> List[str]:
+    response = urlopen(url)
+    xml_data = response.read().decode()
+    root = ElementTree.fromstring(xml_data)
+    return [
+        element.attrib["href"] for element in root.iter() if "href" in element.attrib
+    ]
+
+
+# Similar to https://github.com/richarvey/getAWSdocs/blob/master/getAWSdocs.py
+def get_valid_xml_links(links: List[str]) -> List[str]:
+    pattern = re.compile(r"/([^/]*)/")
+    link_list = []
+    for link in links:
+        # print(f"{href=}")
+        if "?" not in link or link.startswith("https://"):
+            continue
+        url = base_url + link.split("?")[0] + "en_us/" + "landing-page.xml"
+        print(f"{url=}")
+
+        response = requests.get(url)
+        if (
+            response.status_code == 200
+            and "userguide" in response.text.lower()
+            or "developer guide" in response.text.lower()
+        ):
+            # print(href)
+            matches = pattern.match(link)
+            word = matches.group(1) if matches else None
+            if word:
+                print(word)
+            link_list.append(link)
+    with open('xml_links.json', 'w') as _file:
+        json.dump(link_list, _file, indent=4)
+    return link_list
+
+
+def get_developer_and_user_guide_links(links: List[str]) -> List[str]:
+    links = []
+    for link in links:
+        doc_links = get_href_links(link)
+        links.extend(
+            list(
+                filter(
+                    lambda x: "/dg/" in x.lower()
+                    or "userguide" in x.lower()
+                    or "developerguide" in x.lower(),
+                    doc_links,
+                )
+            )
+        )
+    return links
+
+# TOC to find all the user guide HTML links
+def get_toc(links: List[str]) -> List[Dict]:
+    for link in links:
+        url = base_url + link + "toc-contents.json"
+        response = requests.get(url)
+        service = url.split("/")[1]
+        with open(f"toc/{service}.json", "w") as _file:
+            _file.write(response.text)
+            print(f"{service} toc downloaded")
+
+
+def run():
+    links = get_href_links("https://docs.aws.amazon.com/en_us/main-landing-page.xml")
+    valid_xml_pages = get_valid_xml_links(links)
+    devloper_and_ug_links = get_developer_and_user_guide_links(valid_xml_pages)
+    get_toc(devloper_and_ug_links)
+
+
+if __name__ == "__main__":
+    run()
---
+++ aws_service_list/service_dump.py
@@ -0,0 +1,71 @@
+import glob
+import json
+import urllib.request
+import zipfile
+from pathlib import Path
+from typing import Dict, List
+
+# list of services with AWS CLI
+# aws services list
+
+
+def download_github_repo(repo_url: str, output_dir: str, branch: str = "main") -> bool:
+    parts = repo_url.split("/")
+    owner, repo_name = parts[-2], parts[-1]
+
+    zip_url = f"https://codeload.github.com/{owner}/{repo_name}/zip/refs/heads/{branch}"
+
+    output_dir_path = Path(output_dir)
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    zip_path = output_dir_path / f"{repo_name}.zip"
+    urllib.request.urlretrieve(zip_url, str(zip_path))
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(output_dir)
+
+    zip_path.unlink()
+    print(f"Repository {repo_name} downloaded and extracted to {output_dir_path}")
+    return Path(output_dir).exists()
+
+
+def unique_dicts(dicts):
+    seen = set()
+    return [
+        d
+        for d in dicts
+        if not (t := tuple(sorted(d.items()))) in seen and not seen.add(t)
+    ]
+
+
+def get_service_list(file_list: List[str]) -> List[Dict[str, str]]:
+    service_list = []
+    for file_path in file_list:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            service_list.append(
+                {
+                    "fullname": data.get("metadata", {}).get("serviceFullName", ""),
+                    "abbr": data.get("metadata", {}).get("serviceAbbreviation", ""),
+                }
+            )
+    # return list(filter(None, (set(service_list))))
+    return unique_dicts(service_list)
+
+
+def run():
+    repo_name = "botocore"
+    branch = "develop"
+    url = f"https://github.com/boto/{repo_name}"
+    download_github_repo(url, repo_name, branch)
+    file_list = glob.glob(
+        "botocore/botocore-develop/botocore/data/**/service-2.json", recursive=True
+    )
+    service_list = get_service_list(file_list)
+    with open("service_list.json", "w") as _file:
+        json.dump(service_list, _file, indent=4)
+    print("Service list dumped to service_list.json")
+
+
+if __name__ == "__main__":
+    run()
