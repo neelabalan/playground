20ccdcd83d8d9e8accb97393a28e1e49cfc66d8f
neelabalan <neelabalan.n@outlook.com>
2023-11-07 22:43:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+> An attempt at Python monorepo with poetry
+
+![image](https://m.media-amazon.com/images/M/MV5BYmRlZTdmNTEtNmY5Ny00MTlmLTk5M2UtNGQwYzFkYzU2YWRlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg)
+
+
+
+
+
+
+```sh
+docker build -f tools/gradio_app1/Dockerfile .
+```
---
+++ tools/gradio_app1/app.py
@@ -0,0 +1,28 @@
+import gradio as gr
+
+from common.load_data import load_characters
+
+characters = load_characters()
+# Load JSON data into a dictionary for easier access
+character_dict = {character['name']: character for character in characters}
+
+# Define a function to fetch character details
+def get_character_details(character_name):
+    character = character_dict.get(character_name, {})
+    return character
+
+# Create a dropdown menu of character names
+dropdown = gr.Dropdown(choices=list(character_dict.keys()), label="Select a Character")
+
+# Create Gradio interface
+interface = gr.Interface(
+    fn=get_character_details,
+    inputs=dropdown,
+    outputs="json",
+    title="Rick and Morty Character Details",
+    description="Select a character to see their details."
+)
+
+# Run the app
+if __name__ == "__main__":
+    interface.launch(server_name="0.0.0.0", server_port=7860)
