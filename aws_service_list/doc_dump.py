da47bd1ff0831beb7a1d1bce3e59e99e34ce94ca
neelabalan <neelabalan.n@outlook.com>
2023-11-09 16:06:17 +0530

    Changes to include base_url and dump markdown

--- aws_service_list/doc_dump.py
+++ aws_service_list/doc_dump.py
@@ -1,12 +1,14 @@
-import re
-
 import json
-from typing import List
-from typing import Dict
+import pathlib
+import re
+from typing import Dict, List
 from urllib.request import urlopen
 from xml.etree import ElementTree
 
 import requests
+from markdownify import markdownify as md
+from bs4 import BeautifulSoup
+
 
 base_url = "https://docs.aws.amazon.com"
 
@@ -56,9 +58,12 @@ def get_developer_and_user_guide_links(links: List[str]) -> List[str]:
         filtered_links.extend(
             list(
                 filter(
-                    lambda x: (x.lower().endswith("/dg/")
-                    or x.lower().endswith("/userguide/")
-                    or x.lower().endswith("/developerguide/")) and not x.startswith("https://"),
+                    lambda x: (
+                        x.lower().endswith("/dg/")
+                        or x.lower().endswith("/userguide/")
+                        or x.lower().endswith("/developerguide/")
+                    )
+                    and not x.startswith("https://"),
                     doc_links,
                 )
             )
@@ -67,7 +72,7 @@ def get_developer_and_user_guide_links(links: List[str]) -> List[str]:
     with open("developer_and_userguide_links.json", "w") as _file:
         json.dump(filtered_links, _file, indent=4)
     print(f"{len(filtered_links)=}")
-    return filtered_links
+    return list(set(filtered_links))
 
 
 # TOC to find all the user guide HTML links
@@ -79,15 +84,57 @@ def get_toc(links: List[str]) -> List[Dict]:
         # link looks something like this 'https://docs.aws.amazon.com/lambda/latest/dg/toc-contents.json'
         service = link.split("/")[3]
         with open(f"toc/{service}.json", "w") as _file:
-            _file.write(response.text)
+            json.dump(
+                {
+                    "base_url": link,
+                    "service": service,
+                    "toc": json.loads(response.text),
+                },
+                _file,
+                indent=4,
+            )
             print(f"{service} toc downloaded")
 
+def extract_hrefs(data):
+    hrefs = []
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == 'href':
+                hrefs.append(value)
+            else:
+                hrefs.extend(extract_hrefs(value))
+    elif isinstance(data, list):
+        for item in data:
+            hrefs.extend(extract_hrefs(item))
+    return hrefs
+
+def sanitize_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    for script in soup(["script", "style"]):  # remove all javascript and stylesheet code
+        script.extract()
+    return str(soup)
 
 def run():
     links = get_href_links("https://docs.aws.amazon.com/en_us/main-landing-page.xml")
     valid_xml_pages = get_valid_xml_links(links)
     devloper_and_ug_links = get_developer_and_user_guide_links(valid_xml_pages)
     get_toc(devloper_and_ug_links)
+
+    toc_path = pathlib.Path("toc")
+    for path in toc_path.iterdir():
+        with open(path, "r") as _file:
+            data = json.load(_file)
+            hrefs = extract_hrefs(data['toc'])
+            print(hrefs)
+            for href in hrefs:
+                response = requests.get(data["base_url"] + href)
+                markdown = md(sanitize_html(response.text))
+                service_dir = pathlib.Path("markdown_doc") / data["service"]
+                service_dir.mkdir(parents=True, exist_ok=True)
+                with open(
+                    service_dir / f"{pathlib.Path(href).name}.md", "w"
+                ) as md_file:
+                    md_file.write(markdown)
 
 
 if __name__ == "__main__":
