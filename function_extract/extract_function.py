50138e98ec700e1ef0a402bc75f6656935502ff5
neelabalan <neelabalan.n@outlook.com>
2023-11-20 22:52:31 +0530

    Use ast to get functions

---
+++ function_extract/extract_function.py
@@ -0,0 +1,24 @@
+import ast
+import typing
+
+class FunctionExtractor(ast.NodeVisitor):
+    def __init__(self, keywords):
+        self.keywords = keywords
+        self.functions = []
+
+    def visit_FunctionDef(self, node):
+        if all(keyword.lower() in ast.unparse(node).lower() for keyword in self.keywords):
+            self.functions.append(ast.unparse(node))
+        self.generic_visit(node)
+
+def extract_functions(source_code: str, keywords: typing.List[str]):
+    tree = ast.parse(source_code)
+    extractor = FunctionExtractor(keywords)
+    extractor.visit(tree)
+    return extractor.functions
+
+if __name__ == "__main__":
+    with open('test.py', 'r') as _file:
+        source_code = _file.read()
+    print(extract_functions(source_code, ['funny', 'joke']))
+    print(extract_functions(source_code, ['test']))
---
+++ function_extract/test.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def funny_addition(a, b):
+    result = a + b
+    jokes = ["+ It's mathemagical!", "+ Numbers are fun, aren't they?", "+ And the math wizards strike again!"]
+    return f"{result} {random.choice(jokes)}"
+
+
+def funny_password_generator(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password + random.choice(['_haha', '_lol', '_funny'])
+
+def funny_echo(sentence):
+    return sentence.replace(" ", "... ha... ") + " 😂"
+
+def funny_temperature_converter(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return f"{celsius}°C is {fahrenheit}°F. Hot or cold, it's just a state of mind!"
+
+def test():
+    ...
+
+def new_test():
+    ...
