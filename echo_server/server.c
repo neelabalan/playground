e796623d86df09370e6de2addbdb49a7ee705d68
neelabalan <neelabalan.n@outlook.com>
2025-07-27 13:15:04 +0530

    Added clang format and formatted the code

--- echo_server/server.c
+++ echo_server/server.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h> // for memset
-#include <strings.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <getopt.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // for memset
+#include <strings.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 #define DEFAULT_PORT 8080
 
-void print_usage(const char *prog_name) {
+void print_usage(const char* prog_name) {
     printf("Usage: %s [-t|-u] [-p port]\n", prog_name);
     printf("Options:\n");
     printf("  -t              Use TCP protocol (default)\n");
@@ -31,13 +31,17 @@ int start_tcp_server(int sockfd) {
     }
 
     while (1) {
-        int client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+        int client_fd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
         if (client_fd == -1) {
             perror("Error accepting connection");
             continue;
         }
 
-        printf("Connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        printf(
+            "Connection from %s:%d\n",
+            inet_ntoa(client_addr.sin_addr),
+            ntohs(client_addr.sin_port)
+        );
 
         while (1) {
             ssize_t bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
@@ -61,37 +65,46 @@ int start_udp_server(int sockfd) {
     socklen_t client_len = sizeof(client_addr);
 
     while (1) {
-        ssize_t bytes_read = recvfrom(sockfd, buffer, BUFFER_SIZE - 1,
-                                     0, (struct sockaddr *)&client_addr, &client_len);
+        ssize_t bytes_read = recvfrom(
+            sockfd,
+            buffer,
+            BUFFER_SIZE - 1,
+            0,
+            (struct sockaddr*)&client_addr,
+            &client_len
+        );
         if (bytes_read < 0) {
             perror("Error receiving data");
             continue;
         }
         buffer[bytes_read] = '\0';
-        printf("Received from %s:%d: %s\n",
-               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
-        if (sendto(sockfd, buffer, bytes_read, 0,
-                   (struct sockaddr *)&client_addr, client_len) < 0) {
+        printf(
+            "Received from %s:%d: %s\n",
+            inet_ntoa(client_addr.sin_addr),
+            ntohs(client_addr.sin_port),
+            buffer
+        );
+        if (sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr*)&client_addr, client_len) < 0) {
             perror("Error sending data");
         }
     }
 }
 
-void error_message(const char *format, ...) {
+void error_message(const char* format, ...) {
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
     va_end(args);
     // ensure output is written immediately
-    fflush(stderr); 
+    fflush(stderr);
 }
 
 typedef struct {
-    char *transport;
+    char* transport;
     unsigned int port;
 } cmd_args;
 
-cmd_args parse_args(int argc, char *argv[]) {
+cmd_args parse_args(int argc, char* argv[]) {
     int opt;
     int option_index = 0;
     static struct option long_options[] = {
@@ -101,45 +114,43 @@ cmd_args parse_args(int argc, char *argv[]) {
         {0, 0, 0, 0}
     };
     unsigned int port = 8080;
-    char *transport = "tcp";
+    char* transport = "tcp";
 
     while ((opt = getopt_long(argc, argv, "", long_options, &option_index)) != -1) {
         switch (opt) {
-            case 't':
-                transport = optarg;
-                if (strcasecmp(transport, "tcp") !=0 && strcasecmp(transport, "udp") !=0) {
-                    error_message("Invalid trasport protocol. Use 'tcp' or 'udp'");
-                    print_usage(argv[0]);
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'p':
-                port = atoi(optarg);
-                if (port < 1) {
-                    error_message("Port value cannot be less than 0");
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'h':
+        case 't':
+            transport = optarg;
+            if (strcasecmp(transport, "tcp") != 0 && strcasecmp(transport, "udp") != 0) {
+                error_message("Invalid trasport protocol. Use 'tcp' or 'udp'");
                 print_usage(argv[0]);
-                exit(EXIT_SUCCESS);
-            default:
-                print_usage(argv[0]);
-                exit(EXIT_SUCCESS);
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'p':
+            port = atoi(optarg);
+            if (port < 1) {
+                error_message("Port value cannot be less than 0");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'h':
+            print_usage(argv[0]);
+            exit(EXIT_SUCCESS);
+        default:
+            print_usage(argv[0]);
+            exit(EXIT_SUCCESS);
         }
     }
     cmd_args args = {transport, port};
     return args;
-} 
+}
 
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     int sockfd;
     struct sockaddr_in server_addr;
 
     cmd_args args = parse_args(argc, argv);
-    if (strcasecmp(args.transport, "tcp")==0) {
+    if (strcasecmp(args.transport, "tcp") == 0) {
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
     } else {
         sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -155,7 +166,7 @@ int main(int argc, char *argv[]) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(args.port);
 
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error binding socket");
         close(sockfd);
         exit(1);
@@ -163,8 +174,8 @@ int main(int argc, char *argv[]) {
 
     printf("Echo server started on port %d using %s\n", args.port, args.transport);
 
-    if (strcasecmp(args.transport, "tcp")==0) {
-        if(start_tcp_server(sockfd) == -1) {
+    if (strcasecmp(args.transport, "tcp") == 0) {
+        if (start_tcp_server(sockfd) == -1) {
             perror("");
             return -1;
         };
